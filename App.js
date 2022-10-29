3394e8fdac768d9239d1a8c9e1d07acf56436aef
Leeseonyong <qnfto7898@naver.com>
2022-10-29 21:03:21 +0900

    Work-Travel-App

---
+++ App.js
@@ -0,0 +1,144 @@
+import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert, Platform } from 'react-native';
+import { theme } from './colors';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Fontisto } from '@expo/vector-icons';
+
+const STORAGE_KEY = "@toDos";
+export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+  useEffect(() => {
+    loadToDos();
+  }, []);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+  const saveToDos = (toSave) => {
+    AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave))
+  };
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    if (s) {
+      setToDos(JSON.parse(s));
+    }
+  };
+  const addToDo = () => {
+    if (text === "") {
+      return;
+    }
+    const newToDos = {
+      ...toDos,
+      [Date.now()]: { text, working },
+    };
+    setToDos(newToDos);
+    saveToDos(newToDos);
+    setText("");
+  };
+  const deleteToDo = async (key) => {
+    if (Platform.OS === "web") {
+      const ok = confirm("삭제하시겠습니까?")
+      if (ok) {
+        const newToDos = { ...toDos };
+        delete newToDos[key];
+        setToDos(newToDos);
+        saveToDos(newToDos);
+      }
+    } else {
+      Alert.alert("삭제하시겠습니까?", "삭제하시면 다시 복구하실 수 없습니다", [
+        { text: "취소" },
+        {
+          text: "확인",
+          onPress: () => {
+            const newToDos = { ...toDos };
+            delete newToDos[key];
+            setToDos(newToDos);
+            saveToDos(newToDos);
+          }
+        },
+      ]);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{
+              fontSize: 44,
+              color: working ? "white" : theme.grey
+            }}>할 일
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              fontWeight: "600",
+              fontSize: 44,
+              color: !working ? "white" : theme.grey
+            }}>여행
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        onSubmitEditing={addToDo}
+        onChangeText={onChangeText}
+        returnKeyType="done"
+        value={text}
+        placeholder={working ? "할 일 기록 작성란" : "여행 기록 작성란"}
+        style={styles.input}
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <TouchableOpacity onPress={() => deleteToDo(key)}>
+                <Fontisto name="trash" size={18} color={theme.grey} />
+              </TouchableOpacity>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
---
+++ ReadMe.md
@@ -0,0 +1,33 @@
+## 기록장 앱
+
+> 할 일과 여행 카테고리를 나누어 기록할 수 있게끔 만든 앱
+
+
+
+[기록장 앱 구경하러가기](https://expo.dev/@leeseonyong/WorkHardTravelHardApp?serviceType=classic&distribut)
+
+<img src="assets/img_main1.png" width="200" height="400" />
+<img src="assets/img_main2.png" width="200" height="400" />
+<img src="assets/img_main3.png" width="200" height="400" />
+<img src="assets/img_main4.png" width="200" height="400" />
+<img src="assets/img_main5.png" width="200" height="400" />
+<img src="assets/img_main6.png" width="200" height="400" />
+
+
+### Development
+
+<img src="https://img.shields.io/badge/React Native-61DAFB?style=flat-square&logo=React&logoColor=black"/>
+
+### Dependencies
+
+``` json
+"dependencies": {
+    "@react-native-async-storage/async-storage": "~1.17.3",
+    "expo": "~46.0.16",
+    "expo-status-bar": "~1.4.0",
+    "expo-updates": "~0.14.6",
+    "gh-pages": "^4.0.0",
+    "react": "18.0.0",
+    "react-native": "0.69.6"
+  }
+```
